fe0b74d8167c37a0010db61e4ce689b6b58c4644
Mahnoush <36073808+mahnoooosh@users.noreply.github.com>
2019-07-31 23:59:04 -0600

    sta headers and staDataExporter added

---
+++ src/components/data-exporter/staDataExporter.vue
@@ -0,0 +1,81 @@
+<template>
+    <DownloadExcel
+            :fetch="cbFunction"
+            :fields="jsonFields"
+            :type="type"
+            :name="fileName"
+    >
+    <span > Export CSV</span>
+    </DownloadExcel>
+</template>
+
+<script>
+
+    import DownloadExcel from 'vue-json-excel'
+    export default {
+        name: "staDataExporter",
+        components: {
+            DownloadExcel,
+        },
+        props: {
+            cbFunction: {
+                type: Function,
+                required: true,
+            },
+            jsonFields: {
+                type: Object,
+                required: true,
+            },
+            jsonData: {
+                type: Array,
+                default() {
+                    return []
+                },
+            },
+            jsonMeta: {
+                type: Array,
+                default() {
+                    return [
+                        [
+                            {
+                                key: 'charset',
+                                value: 'utf-8',
+                            },
+                        ],
+                    ]
+                },
+            },
+            title: {
+                type: String,
+                default: 'Example CSV',
+            },
+            name: {
+                type: String,
+                default: 'asset_history_data',
+            },
+            type: {
+                type: String,
+                default: 'csv',
+            },
+            icon: {
+                type: String,
+                default: 'fa-file-export',
+            },
+        },
+        computed: {
+            fileName() {
+
+                return this.name + '.' + this.type
+            },
+        },
+        methods: {
+            beforLoading(){ // before-generate
+                alert('show loading');
+            },
+            finish(){ // before-finish
+                alert('finish');
+            },
+        }
+
+    }
+</script>
